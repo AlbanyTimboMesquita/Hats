c31b0232d21be4a5147cd0f3770dcf70498adb93
Albany Timbó Mesquita <4lb4ny@gmail.com>
2024-02-16 18:55:06 -0300

    Ganhando pontos ao coletar bolas

---
+++ Hats/Assets/_Game/Scripts/GameController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+   public int score;
+    void Start()
+    {
+        score=0;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Hats/Assets/_Game/Scripts/MyTrigger.cs
+++ Hats/Assets/_Game/Scripts/MyTrigger.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class MyTrigger : MonoBehaviour
 {
+    private GameController gameController;
+
+    private void Start() {
+        gameController = FindObjectOfType<GameController>();
+    }
     private void OnTriggerEnter2D(Collider2D target) {
         if(target.gameObject.CompareTag("Destroyer")){
             Destroy(this.gameObject);
+        }else if(target.gameObject.CompareTag("Point")){
+                gameController.score+=1;
+                Destroy(this.gameObject);
         }
         
     }
