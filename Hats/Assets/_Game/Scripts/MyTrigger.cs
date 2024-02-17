5e2bd74008705c05f8c0fdf8f0392d6baee7a768
Albany Timbó Mesquita <4lb4ny@gmail.com>
2024-02-17 20:28:08 -0300

    Final 1.0

--- Hats/Assets/_Game/Scripts/MyTrigger.cs
+++ Hats/Assets/_Game/Scripts/MyTrigger.cs
@@ -6,16 +6,20 @@ public class MyTrigger : MonoBehaviour
 {
     private GameController gameController;
     private UiController uiController;
+    public AudioSource audioPlayer;
 
     private void Start() {
         gameController = FindObjectOfType<GameController>();
         uiController = FindObjectOfType<UiController>();
+        audioPlayer = FindObjectOfType<AudioSource>();
+        
     }
     private void OnTriggerEnter2D(Collider2D target) {
         if(target.gameObject.CompareTag("Destroyer")){
             Destroy(this.gameObject);
         }else if(target.gameObject.CompareTag("Point")){
                 gameController.score+=23;
+                audioPlayer.Play();
                 uiController.txtScore.text = gameController.score.ToString();
                 Destroy(this.gameObject);
         }
